5619cf69e11a8ebbc139c2f79cf21eb018fd3fac
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-02-17 12:34:19 -0300

    Instalação do crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:alura_crashlytics/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(BytebankApp());
 }
 
