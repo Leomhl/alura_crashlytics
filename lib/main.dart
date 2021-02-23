621174615704c14e9e5c36daa92d4c7700426b49
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-02-22 22:03:01 -0300

    Teste de debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:alura_crashlytics/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  FirebaseCrashlytics.instance.setUserIdentifier('alura123');
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    FirebaseCrashlytics.instance.setUserIdentifier('alura123');
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
 
   runApp(BytebankApp());
 }
